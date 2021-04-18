7f49044dc5daef35cbc197209253a43f2003a9cc
Kakashi-23 <64219152+Kakashi-23@users.noreply.github.com>
2021-04-18 14:30:26 +0530

    Screen basic layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
-
+import 'package:world_time_app/Screen/choose_location.dart';
+import 'package:world_time_app/Screen/loading.dart';
+import 'Screen/home.dart';
 void main() {
-  runApp();
+  runApp(MaterialApp(
+    initialRoute: '/home',
+    routes: {
+      '/':(context) => Loading(),
+      '/home':(context) => Home(),
+      '/location': (context) => ChooseLocation(),
+    },
+  ));
 }
 
